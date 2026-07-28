c02dffd712795dad55b06b696304d7d6f9a3a62b
Dhanushree292 <192421218.simats@saveetha.com>
2026-07-28 20:50:17 +0530

    Create EXP-23 Top hat technique as a Morphological operation.py

---
+++ EXP-23 Top hat technique as a Morphological operation.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("image.jpg")
+
+kernel = np.ones((5,5), np.uint8)
+
+
+tophat = cv2.morphologyEx(image, cv2.MORPH_TOPHAT, kernel)
+
+cv2.imshow("Original Image", image)
+cv2.imshow("Top Hat", tophat)
+
+cv2.imwrite("tophat_image.jpg", tophat)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
