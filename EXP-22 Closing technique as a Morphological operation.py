aff6206427a1711039486c33fe3584ea67c81171
Dhanushree292 <192421218.simats@saveetha.com>
2026-07-28 20:48:09 +0530

    Create EXP-22 Closing technique as a Morphological operation.py

---
+++ EXP-22 Closing technique as a Morphological operation.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("image.jpg")
+
+kernel = np.ones((5,5), np.uint8)
+
+closing = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+
+cv2.imshow("Original Image", image)
+cv2.imshow("Closing", closing)
+
+cv2.imwrite("closing_image.jpg", closing)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
